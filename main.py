24052473b16372bae0b9b9e25b58aeb146178eb4
Aaditya Kalwani <64959469+aadityakalwani@users.noreply.github.com>
2022-06-04 13:46:30 +0000

    included a while loop to keep running the game until specified not to

--- main.py
+++ main.py
@@ -16,7 +16,9 @@ def play():
 def is_win(player, opponent):
   if player == "r" and opponent == "s" or player == "p" and opponent == "r" or player == "s" and opponent == "p":
     return True
-    
-# while loop that says 'while game is not won'
 
-print(play())
+
+play_or_not = ""
+while play_or_not.lower() != "q":
+  print(play())
+  play_or_not = input("enter 'q' to quit playing, anything else assumes that you want to continue playing\n")
