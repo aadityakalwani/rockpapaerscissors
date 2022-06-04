40641a8b7efbbac105c080ed12e3567c982193dc
Aaditya Kalwani <64959469+aadityakalwani@users.noreply.github.com>
2022-06-04 13:39:37 +0000

    completed the game, included a win draw and loss checker

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import random
 
 def play():
-  player_turn = (input("Enter the move you want to make\ntype 'r' for rock, 'p' for paper and 's' for scissors:\n"))
-  computer_turn = random.choice(['r', 'p', 's'])
-  print(f"the computer chose {computer_turn}")
+  user = (input("Enter the move you want to make\ntype 'r' for rock, 'p' for paper and 's' for scissors:\n"))
+  computer = random.choice(["r", "p", "s"])
+  print(f"the computer chose {computer}")
 
-  if player_turn == computer_turn:
+  if user == computer:
     return "draw"
 
+  if is_win(user,computer):
+    return "you won!"
+
+  return "you lost huge L"
+
 def is_win(player, opponent):
-  
+  if player == "r" and opponent == "s" or player == "p" and opponent == "r" or player == "s" and opponent == "p":
+    return True
     
 # while loop that says 'while game is not won'
 
-play()
+print(play())
