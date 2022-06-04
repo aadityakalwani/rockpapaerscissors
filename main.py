bce83b68f868c7898354268d54dfea8c1b1e88ab
Aaditya Kalwani <64959469+aadityakalwani@users.noreply.github.com>
2022-06-04 13:32:09 +0000

    defined a function to check for a win

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ def play():
   player_turn = (input("Enter the move you want to make\ntype 'r' for rock, 'p' for paper and 's' for scissors:\n"))
   computer_turn = random.choice(['r', 'p', 's'])
   print(f"the computer chose {computer_turn}")
-  win_or_lose = ""
 
   if player_turn == computer_turn:
     return "draw"
 
+def is_win(player, opponent):
+  
     
 # while loop that says 'while game is not won'
 
